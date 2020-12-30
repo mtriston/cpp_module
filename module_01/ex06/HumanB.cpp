a68519dd91626b9c34c94c6ff814c08233c78131
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-12-30 16:46:04 +0300

    ex06 done

---
+++ module_01/ex06/HumanB.cpp
@@ -0,0 +1,14 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name) : _name(name) {}
+
+void	HumanB::setWeapon(Weapon& weapon)
+{
+	this->_weapon = &weapon;
+}
+
+void	HumanB::attack()
+{
+	std::cout << _name << " attacks with his ";
+	std::cout << (*_weapon).getType() << std::endl;
+}
--- module_01/ex06/main.cpp
+++ module_01/ex06/main.cpp
@@ -1,4 +1,5 @@
 #include "HumanA.hpp"
+#include "HumanB.hpp"
 
 int main()
 {
@@ -9,6 +10,14 @@ int main()
 		bob.attack();
 		club.setType("some other type of club");
 		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
 	}
 	return (0);
 }
