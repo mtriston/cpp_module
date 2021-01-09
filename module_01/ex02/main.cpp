37c902a3668035888d80819e1a7b44f81d097fda
Alexander Spivak <po4ta.polevaya@gmail.com>
2021-01-09 18:16:13 +0300

    ex07 done

--- module_01/ex02/main.cpp
+++ module_01/ex02/main.cpp
@@ -5,9 +5,11 @@ int		main()
 	ZombieEvent spawn;
 	Zombie		*zombie;
 
+	spawn.setZombieType("Deadly");
 	zombie = spawn.newZombie("Jack");
 	zombie->announce();
 	delete zombie;
 	zombie = spawn.randomChump();
+	delete zombie;
 	return (0);
 }
--- module_01/ex07/main.cpp
+++ module_01/ex07/main.cpp
@@ -1,17 +1,43 @@
 #include <fstream>
 #include <iostream>
 
+int	error(std::string msg)
+{
+	std::cout << "Error!" << std::endl;
+	std::cout << msg << std::endl;
+	return (1);
+}
+
 int main(int argc, char const *argv[])
 {
-	std::ofstream src;
-	std::ofstream dst;
+	std::ifstream	src;
+	std::ofstream	dst;
+	std::string		buf;
+	std::string		s1;
+	std::string		s2;
+
 	if (argc != 4)
-	{
-		std::cout << "Error\nInvalid count of arguments" << std::endl;
-	}
+		return (error("Invalid count of arguments."));
+	if (!*(argv[2]) || !*(argv[3]))
+		return (error("Empty argument."));
+	s1 = argv[2];
+	s2 = argv[3];
 	src.open(argv[1]);
-	
-
-
+	if (!src.is_open())
+		return (error("The src file cannot be opened."));
+	dst.open(std::string (argv[1]) + ".replace");
+	if (!dst.is_open())
+		return (error("The dst file cannot be opened."));
+	do
+	{
+		if (buf.size() > 0)
+			dst << ' ';
+		if (buf == s1)
+			dst << s2;
+		else
+			dst << buf;
+	} while (src >> buf);
+	src.close();
+	dst.close();
 	return 0;
 }
