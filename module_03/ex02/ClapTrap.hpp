72fd99a1867eb69b50982ae0a61a833b15bdd94a
Alexander Spivak <po4ta.polevaya@gmail.com>
2021-01-17 21:40:07 +0300

    m3 ex2 done

---
+++ module_03/ex02/ClapTrap.hpp
@@ -0,0 +1,33 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <string>
+# include <iostream>
+
+class ClapTrap
+{
+protected:
+	unsigned	_hitPoints;
+	unsigned   	_maxHP;
+	unsigned	_energyPoints;
+	unsigned	_maxEP;
+	unsigned	_level;
+	unsigned	_meleeAttackDmg;
+	unsigned	_rangedAttackDmg;
+	unsigned	_armorReduction;
+    std::string _name;
+
+public:
+	ClapTrap();
+	ClapTrap(std::string const &name);
+	ClapTrap(ClapTrap const &);
+	ClapTrap & operator = (ClapTrap const &);
+	virtual ~ClapTrap();
+
+	void    rangedAttack(std::string const &target);
+	void    meleeAttack(std::string const &target);
+	void    takeDamage(unsigned int amount);
+	void    beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ module_03/ex02/main.cpp
@@ -0,0 +1,48 @@
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
+
+int main()
+{
+    std::cout << "---------FragTrap----------" << std::endl;
+    {
+        FragTrap a;
+        FragTrap b("b");
+        FragTrap c(a);
+        a = FragTrap("c");
+
+        a.meleeAttack("pig");
+        b.rangedAttack("donkey");
+        a.takeDamage(110);
+        c.takeDamage(5);
+        c.takeDamage(20);
+        c.beRepaired(30);
+
+        c.vaulthunter_dot_exe("Monkey");
+        c.vaulthunter_dot_exe("Elephant");
+        c.vaulthunter_dot_exe("Stranger");
+        c.vaulthunter_dot_exe("Baby");
+        c.vaulthunter_dot_exe("Apple");
+    }
+
+    std::cout << "---------ScavTrap----------" << std::endl;
+    {
+        ScavTrap a;
+        ScavTrap b("ScavBot");
+        ScavTrap c(a);
+        a = ScavTrap("Bender");
+
+        a.meleeAttack("pig");
+        b.rangedAttack("donkey");
+        a.takeDamage(110);
+        c.takeDamage(5);
+        c.takeDamage(20);
+        c.beRepaired(30);
+
+        c.challengeNewcomer("Monkey");
+        c.challengeNewcomer("Elephant");
+        c.challengeNewcomer("Stranger");
+        c.challengeNewcomer("Baby");
+        c.challengeNewcomer("Apple");
+    }
+    return 0;
+}
